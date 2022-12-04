83a8a7a7183148d6b1011c7bc81bf91c81273e5a
Artem Zubov <artmzbv@gmail.com>
2022-12-04 20:05:00 +0300

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/users', require('./routes/users'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on ports${PORT}`);
+});
