9c13239c0a44976ade2864bfa86912dec1728995
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-12-18 01:23:56 +0300

    Add files via upload

--- routes/cards.js
+++ routes/cards.js
@@ -1,5 +1,3 @@
-/* eslint-disable linebreak-style */
-// /* eslint-disable linebreak-style */
 const router = require('express').Router();
 const {
   createCard, getCards, deleteCardById, likeCard, dislikeCard,
