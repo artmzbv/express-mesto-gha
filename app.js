c4b75b1cc3fb24d6886c7742c53a452644398de3
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-12-17 13:56:44 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
 
-const { PORT = 3000 } = process.env;
+mongoose.set('strictQuery', false);
 
 const app = express();
+const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-
 app.use('/users', require('./routes/users'));
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '63965591a3e2e5473bdc580e',
+  };
+
+  next();
+});
+
+app.use('/', userRouter);
+app.use('/', cardRouter);
+app.use((req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
+
 app.listen(PORT, () => {
-  console.log(`App listening on ports${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
