93453dbc6fe81b3de9202def1cf57176e2ba2709
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-12-18 01:24:21 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-/* eslint-disable linebreak-style */
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -13,8 +12,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use('/users', require('./routes/users'));
 
 app.use((req, res, next) => {
   req.user = {
