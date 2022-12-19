0742bce0677c1f380324a6dab50906629a9980a0
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-12-19 11:12:07 +0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
 const router = require('express').Router();
 const {
-  getUserById, getUsers, updateProfile, updateAvatar,
+  getUserById, getUsers, updateProfile, updateAvatar, getProfile,
 } = require('../controllers/users');
 const { validatedUserId, validatedProfile, validatedAvatar } = require('../utils/validation');
 
 router.get('/users', getUsers);
 
+router.get('users/me', getProfile);
+
 router.get('/users/:id', validatedUserId, getUserById);
 
 // router.post('/users', postUser);
 
-router.patch('/users/me', updateProfile);
-
-router.patch('/me', validatedProfile, updateProfile);
+router.patch('/users/me', validatedProfile, updateProfile);
 
 router.patch('/users/me/avatar', validatedAvatar, updateAvatar);
 
