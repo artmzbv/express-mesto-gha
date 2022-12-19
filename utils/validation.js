486e8275299f24e710671411e6c67a7c6d0afbb4
artmzbv <58895197+artmzbv@users.noreply.github.com>
2022-12-19 05:01:00 +0300

    Add files via upload

---
+++ utils/validation.js
@@ -0,0 +1,54 @@
+const { celebrate, Joi } = require('celebrate');
+
+const limitedRef = /^(https?:\/\/)(www\.)?([\w-.~:/?#[\]@!$&')(*+,;=]*\.?)*\.{1}[\w]{2,8}(\/([\w-.~:/?#[\]@!$&')(*+,;=])*)?/;
+
+const validatedUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(limitedRef),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validatedCard = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().required().pattern(limitedRef),
+  }),
+});
+
+const validatedCardId = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex().required(),
+  }),
+});
+
+const validatedProfile = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    about: Joi.string().min(2).max(30).required(),
+  }),
+});
+
+const validatedUserId = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+});
+
+const validatedAvatar = celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(limitedRef),
+  }),
+});
+
+module.exports = {
+  validatedUser,
+  validatedUserId,
+  validatedProfile,
+  validatedAvatar,
+  validatedCard,
+  validatedCardId,
+};
